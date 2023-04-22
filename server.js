3d74a5a179cf749002e823b2cb852603609a7ca2
Fasina Ayomikun <ayomikunfasina240@gmail.com>
2023-04-22 15:36:46 +0100

    fix cors

--- server.js
+++ server.js
@@ -44,65 +44,20 @@ app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
-require("dotenv").config();
-
-let users = [];
-
-const addUser = (userId, socketId) => {
-  !users.some((user) => user.userId === userId) &&
-    users.push({ userId, socketId });
-};
-
-const removeUser = (socketId) => {
-  users = users.filter((user) => user.socketId !== socketId);
-};
-const getUser = (userId) => {
-  return users.find((user) => user.userId === userId);
-};
-
 app.use("/api/v1/user", require("./routes/user"));
 app.use("/api/v1/contacts", require("./routes/contacts"));
 app.use("/api/v1/conversation", require("./routes/conversation"));
 app.use("/api/v1/messages", require("./routes/messages"));
 app.use("/api/v1/upload", require("./routes/files"));
 app.use(notFoundMiddleware);
-
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log("server listening");
+    });
   } catch (error) {
     console.log(error);
   }
 };
-
-const server = app.listen(PORT, () => {
-  console.log("server listening");
-});
 start();
-const io = require("socket.io")(server, {
-  cors: {
-    origin: process.env.FRONTEND_LINK,
-    credentials: true,
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log("user connected");
-
-  socket.on("addUser", (userId) => {
-    addUser(userId, socket.id);
-    io.emit("getUsers", users);
-  });
-  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
-    const user = getUser(receiverId);
-    io.to(user.socketId).emit("getMessage", {
-      senderId,
-      text,
-    });
-  });
-  socket.on("disconnect", () => {
-    console.log("a user disconnected");
-    removeUser(socket.id);
-    io.emit("getUsers", users);
-  });
-});
